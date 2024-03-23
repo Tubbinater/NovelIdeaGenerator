2b6fe721e408a89b34eb3501bc5dad73d733e9c9
Tubbinater <164660442+Tubbinater@users.noreply.github.com>
2024-03-23 12:35:29 -0400

    data.js

--- ideagenerator/data.js
+++ ideagenerator/data.js
@@ -27,18 +27,18 @@ data = `
 ----------------- TEMPLATE ----------------- 
 
 #template:
-<a> @mood@ @genre@ game about @theme@ and @theme@, @wildcard@
-<a> @mood@ @genre@ game @wildcard@ where you play as @character@ @goal@
-<a> @mood@ @genre@ game about @theme@, where you play as @character:nopost@ @setting@
-<a> @mood@ @genre@ game that takes place @setting@, where you play as @character@ @goal@
-<a> @mood@ game about @theme@ [set, that takes place] @setting@, where you play as @character@ @goal@
-<a> @mood@ [mix between, mashup of, blend of] the @genre:nomods@ and @genre:nomods@ game genres, where you play as @character@ @goal@
-<a> @mood@ [mix between, mashup of, blend of] the @genre:nomods@ and @genre:nomods@ game genres, which tells a story of @theme@ and @theme@
-<a> @mood@ @genre@ game @wildcard:always@
-<a> @mood@ [mix between, mashup of, blend of] the @genre:nomods@ and @genre:nomods@ game genres @wildcard:always@
+<a> @mood@ @genre@ story about @theme@ and @theme@, @wildcard@
+<a> @mood@ @genre@ story @wildcard@ where you play as @character@ @goal@
+<a> @mood@ @genre@ story about @theme@, where you play as @character:nopost@ @setting@
+<a> @mood@ @genre@ story that takes place @setting@, where you play as @character@ @goal@
+<a> @mood@ story about @theme@ [set, that takes place] @setting@, where you play as @character@ @goal@
+<a> @mood@ [mix between, mashup of, blend of] the @genre:nomods@ and @genre:nomods@ story genres, where you play as @character@ @goal@
+<a> @mood@ [mix between, mashup of, blend of] the @genre:nomods@ and @genre:nomods@ story genres, which tells a story of @theme@ and @theme@
+<a> @mood@ @genre@ story @wildcard:always@
+<a> @mood@ [mix between, mashup of, blend of] the @genre:nomods@ and @genre:nomods@ story genres @wildcard:always@
 #end
 
------------------ GENRE (a ... game) ----------------- 
+----------------- GENRE (a ... story) ----------------- 
 #genre:
 platformer
 metroidvania
